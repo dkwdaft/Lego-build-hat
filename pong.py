b4a84b686e973fb69475fc0b176149dbba99aef8
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2022-12-30 15:59:47 +0000

    update pong.py (add ball speed)

--- pong.py
+++ pong.py
@@ -20,5 +20,9 @@ paddle_left.shape('square')
 paddle_left.shapesize(4, 1, 1)
 paddle_left.penup()
 paddle_left.setpos(-190, 0)
+ball.speed_x = 1
+ball.speed_y = 1
 while True:
     game_area.update()
+    ball.setx(ball.xcor() + ball.speed_x)
+    ball.sety(ball.ycor() + ball.speed_y)

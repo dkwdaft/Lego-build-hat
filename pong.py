9e14b17dbe07e3704bc5ab3df398263435219430
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2022-12-30 15:56:49 +0000

    update pong.py (add left paddle)

--- pong.py
+++ pong.py
@@ -13,5 +13,12 @@ ball.shape('circle')
 ball.penup()
 ball.setpos(0, 0)
 
+
+paddle_left = Turtle()
+paddle_left.color('green')
+paddle_left.shape('square')
+paddle_left.shapesize(4, 1, 1)
+paddle_left.penup()
+paddle_left.setpos(-190, 0)
 while True:
     game_area.update()

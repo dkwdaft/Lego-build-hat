69d1ecb6efff30466bce6b0709a683e7dc7af142
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2022-12-30 15:52:32 +0000

    update pong.py

--- pong.py
+++ pong.py
@@ -7,5 +7,11 @@ game_area.title("POONG")
 game_area.bgcolor("black")
 game_area.tracer(0)
 game_area.setworldcoordinates(-200, -170, 200, 170)
+ball = Turtle()
+ball.color('white')
+ball.shape('circle')
+ball.penup()
+ball.setpos(0, 0)
+
 while True:
     game_area.update()

ca8fa87cf465041c7873d7ca39fa8ef6ff17fa0c
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2022-12-30 16:03:43 +0000

    update pong.py (update ball speed)

--- pong.py
+++ pong.py
@@ -1,6 +1,6 @@
-from turtle import  Screen, Turtle
-from  time import sleep
-from  buildhat import  Motor
+from turtle import Screen, Turtle
+from time import sleep
+from buildhat import Motor
 
 game_area = Screen()
 game_area.title("POONG")
@@ -12,7 +12,6 @@ ball.color('white')
 ball.shape('circle')
 ball.penup()
 ball.setpos(0, 0)
-
 
 paddle_left = Turtle()
 paddle_left.color('green')
